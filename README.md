ade1f7e8dde3e4df046afb7b9f8562c1477ba20e
jcdeccechis <jcdeccechis@technisys.com>
2021-09-16 15:11:29 -0300

    tp goLang

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # GoLangTP
+
+##Trabajo Entregable GoLang
+El trabajo consiste en desestructurar una entrada de tipo String, y a partir de ella generar una estructura (utilizando Go)
+
+##Implementacion
+Para el desarrollo se separa la entrada en las distintas partes de la estructura (caracteres 0 y 1 son el tipo, 2 y 3 son el tamaño, y el resto es el contenido o valor de la estructura)
+Se realizan validaciones de tipo INT para los campos del tamaño, y para el valor/contenido sólo si el tipo es numérico (NN), y se valida que el tamaño del contenido sea el especificado.
+
+##Ejecucion
+Para la ejecucion, se debe clonar el repositorio en algun destino que permita ejecutar Go.
+Luego abrir un terminal dentro de la carpeta clonada y ejecutar el archivo .go con un comando como el siguiente:
+
+go run tp.go TX03TPF
---
+++ tp.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+type Cadena struct {
+	Input string
+}
+
+type Estructura struct {
+	tipo      string
+	tamaño    int
+	contenido string
+}
+
+func (c *Cadena) GenerarEstructura(contenido string) Estructura {
+	tipo := string(contenido[0]) + string(contenido[1])
+
+	tamañoD, err := strconv.Atoi(string(contenido[2])) // + string(args.Input[3])
+	tamañoU, err := strconv.Atoi(string(contenido[3])) // + string(args.Input[3])
+	tamañoF := 0
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(2)
+	} else {
+		tamañoD = tamañoD * 10
+		tamañoF = tamañoD + tamañoU
+	}
+
+	value := ""
+	for i := 4; i < len(contenido); i++ {
+		value = value + string(contenido[i])
+	}
+
+	est := Estructura{tipo, tamañoF, value}
+	return est
+}
+
+func (e *Estructura) ValidarTamaño() bool {
+	return e.tamaño == len(e.contenido)
+}
+
+func (e *Estructura) ValidarNumero(tipo string) bool {
+	for i := 0; i < len(tipo); i++ {
+		value, err := strconv.Atoi(string(tipo[i]))
+		if err != nil {
+			fmt.Println("ERROR, el valor " + tipo + " no es un número ")
+			fmt.Println(err)
+			os.Exit(2)
+		} else {
+			if !((0 <= value) && (value <= 9)) {
+				fmt.Println("ERROR, el valor " + tipo + " no es un número ")
+				fmt.Println(err)
+				os.Exit(2)
+			}
+		}
+	}
+	fmt.Println("tipo válido para el valor: " + tipo)
+	return true
+}
+
+/*func (e *Estructura) GenerarTamaño(tipo string) int {
+	tamaño := 0
+	fmt.Println("generar tamaño")
+	fmt.Println(tipo)
+	fmt.Println(len(tipo))
+	for i := 0; i < len(tipo); i++ {
+		value, err := strconv.Atoi(string(tipo[i]))
+		if err != nil {
+			fmt.Println("ERROR, el valor " + tipo + " no es un número ")
+			fmt.Println(err)
+			os.Exit(2)
+		} else {
+			if !((0 <= value) && (value <= 9)) {
+				fmt.Println("ERROR, el valor " + tipo + " no es un número ")
+				fmt.Println(err)
+				os.Exit(2)
+			} else {
+				//tamaño = tamaño + value*10 ^ (len(tipo) - (i + 1))
+				fmt.Println(10 ^ (len(tipo) - (i + 1)))
+			}
+		}
+	}
+	e.tamaño = tamaño
+	fmt.Println("fin generar")
+	return tamaño
+}*/
+
+func main() {
+	contenido := Cadena{os.Args[1]}
+	estructura := contenido.GenerarEstructura(contenido.Input)
+	validaTipo := estructura.ValidarNumero(strconv.Itoa(estructura.tamaño))
+	if validaTipo {
+		fmt.Println("tipo size OK")
+	} else {
+		fmt.Println("err: size no es del tipo correcto")
+	}
+	if estructura.tipo == "NN" {
+		validaTipoContenido := estructura.ValidarNumero(string(estructura.contenido))
+		if validaTipoContenido {
+			fmt.Println("tipo size OK")
+		} else {
+			fmt.Println("err: size no es del tipo correcto")
+		}
+	}
+	validaTamaño := estructura.ValidarTamaño()
+	if validaTamaño {
+		fmt.Println("tamaño OK")
+	} else {
+		fmt.Println("err: size y tamaño de contenido no coinciden")
+	}
+
+	fmt.Println(estructura)
+}
